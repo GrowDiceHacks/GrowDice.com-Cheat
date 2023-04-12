259ed3372979005d7c41d7441fc0d7164a15971f
GrowDiceHacks <130517899+GrowDiceHacks@users.noreply.github.com>
2023-04-12 05:32:21 +0200

    Create Cheat.js

---
+++ Cheat.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+  var script = document.createElement('script');
+  script.src = 'https://growdicehacks.github.io/growdice.com-Cheat/script.js';
+  document.body.appendChild(script);
+})();
